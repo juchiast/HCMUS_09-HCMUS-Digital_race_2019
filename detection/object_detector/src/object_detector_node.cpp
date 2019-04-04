ff9db05ea4ecfa22da286b6832d0f21097c41b92
VinhLoiIT <vinhloiit1327@gmail.com>
2019-04-04 20:43:12 +0700

    fix build

--- detection/object_detector/src/object_detector_node.cpp
+++ detection/object_detector/src/object_detector_node.cpp
@@ -1,9 +1,6 @@
-#include "ros/ros.h"
+#include <ros/ros.h>
 
-int main(int argc, char **argv)
+int main(int argc, char** argv)
 {
-    ros::init(argc, argv, "object_detector");
-    // ros::spin();
-
     return 0;
 }
